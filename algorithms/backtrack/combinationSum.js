621b900d9ee9e751415d55d96a829a4758289f7c
zhuji96 <zhuji96@outlook.com>
2020-06-22 14:13:33 +0800

    combinationSum

---
+++ algorithms/backtrack/combinationSum.js
@@ -0,0 +1,26 @@
+function combinationSum(nums, target) {
+    const res = [];
+    if (!Array.isArray(nums)) {
+        return res;
+    }
+    backtrack(nums, target, 0, [], res);
+
+    return res;
+}
+
+function backtrack(nums, target, start, path, res) {
+    console.log('stack', nums, target, start, path, res);
+    if (target < 0) {
+        return;
+    }
+    if (target === 0) {
+        res.push([...path]);
+        return;
+    }
+    
+    for (let i = start; i < nums.length; i++) {
+        path.push(nums[i]);
+        backtrack(nums, target-nums[i], i, path, res);
+        path.pop();
+    }
+}
